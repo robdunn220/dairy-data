ef7623d87846bfe51db2055ef4d4898e1d000b81
robdunn220 <robertdunn220@gmail.com>
2020-06-23 10:09:07 -0400

    Fixed hamburger menu padding issue

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const events = require('./events');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'robert',
+    password: 'robertsql7',
+    database: 'dairy_data_db'
+});
+
+connection.connect();
+
+const port = process.env.PORT || 8080;
+
+const app = express()
+    .use(cors())
+    .use(bodyParser.json())
+    .use(events(connection));
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
+
+connection.query('SELECT * FROM heifers', (err,rows) => {
+    if(err) throw err;
+
+    console.log(rows);
+});
+
+connection.end();
