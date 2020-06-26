236d00ee6a36b82b4c6223c8e7173b995d0715b7
robdunn220 <robertdunn220@gmail.com>
2020-06-26 14:11:25 -0400

    Refactored, changed paragraph text

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['../home/home.component.css'] /* About and home using same styling */
 })
 export class AboutComponent implements OnInit {
 
