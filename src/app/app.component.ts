45df00760ba1551a553071835d6b644c9dea8e19
robdunn220 <robertdunn220@gmail.com>
2020-06-29 10:58:06 -0400

    Compressed about page background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dairy-data';
+  urlPath = 'url(../assets/standard_background.webp)';
+
+  constructor(location: Location, private router: Router) {
+    router.events.subscribe((val) => {
+      // see also
+      if (location.path() === '/about') {
+        this.urlPath = 'url(../assets/about_background.webp)';
+      }
+    });
+  }
 }
